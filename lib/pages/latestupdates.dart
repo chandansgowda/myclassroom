14bc250c6f0d3e4bb7bb1182bd676ff73da445ea
Chandan Gowda <chandansuresh007@gmail.com>
2021-02-10 21:49:15 +0530

    latest updates added

---
+++ lib/pages/latestupdates.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LatestUpdates extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Latest Updates'),
+    );
+  }
+}
